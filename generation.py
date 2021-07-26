a8487f5052bbcf7b4d16d84ac452cb138b90e2d7
ThePythonGuy <45978337+ThePythonGuy3@users.noreply.github.com>
2021-07-26 15:59:48 +0200

    roomgen

--- generation.py
+++ generation.py
@@ -1,16 +1,20 @@
+# Generation by ThePythonGuy3
 from math import *
 from random import *
 from perlin_noise import PerlinNoise
 import os
 
-tiles = os.listdir("map")
+def stringMap(l):
+	o = ""
+	for i in l:
+		for j in i:
+			o += str(j)
+		o += "\n"
+
+	return o
 
 def printMap(l):
-	for i in l:
-		o = ""
-		for j in i:
-			o += str(j) * 2
-		print(o)
+	print(stringMap(l))
 
 def dist(x, y, x2, y2):
 	return sqrt(abs(x - x2) ** 2 + abs(y - y2) ** 2)
@@ -37,6 +41,62 @@ def copyMap(a):
 
 """
 Codes:
+0 - Ground
+1 - Water
+2 - Wall
+3 - Breakable wall
+4 - Bushes
+
+Types:
+shore - used in shores
+water - used as sea
+"""
+
+def generateRoom(size = (20, 16), shore = False, water = False):
+	w, h = size
+
+	shoreAdd = int(shore) * 0.3
+
+	room = []
+	for i in range(h):
+		room.append([])
+		for j in range(w):
+			room[i].append(0)
+
+	# Creating tile noises
+	wNoise = PerlinNoise(octaves = 10, seed = randint(1, 50))
+	bwNoise = PerlinNoise(octaves = 10, seed = randint(1, 50))
+	bNoise = PerlinNoise(octaves = 10, seed = randint(1, 50))
+	wtNoise = PerlinNoise(octaves = 10, seed = randint(1, 50))
+
+	# Full water tile
+	default = 4
+	if water: default = 1
+
+	# Outer rim
+	for i in range(h):
+		for j in range(w):
+			if bNoise([j / w, i / h]) >= 0.1:
+				room[i][j] = default
+
+	if water: return room
+
+	# Inner room
+	for i in range(1, h - 1):
+		for j in range(1, w - 1):
+			if wtNoise([j / w, i / h]) >= 0.3 - shoreAdd:
+				room[i][j] = 1
+
+			if wNoise([j / w, i / h]) >= 0.1:
+				room[i][j] = 2
+
+			if bwNoise([j / w, i / h]) >= 0.1:
+				room[i][j] = 3
+
+	return room
+
+"""
+Codes:
 0 - Sea
 1 - Plains
 2 - Desert
@@ -49,7 +109,7 @@ def generateMap(size, dungeons):
 	w = size
 	h = size
 
-	# Generating biome noises
+	# Creating biome noises
 	dNoise = PerlinNoise(octaves = 10, seed = randint(1, 50))
 	iNoise = PerlinNoise(octaves = 10, seed = randint(1, 50))
 
@@ -97,5 +157,32 @@ def generateMap(size, dungeons):
 				newMapList[i][j] = 4
 
 	return newMapList
+
+# One-time process, takes a long time
+def generateRooms(dir = "rooms"):
+	if not os.path.exists(dir):
+		os.system("mkdir rooms")
+
+	id = 0
+	for i in range(120):
+		f = open(dir + "/" + str(id) + ".room", "w+")
+
+		f.write(stringMap(generateRoom()))
+
+		f.close()
+		id += 1
+
+	id = 0
+	for i in range(20):
+		f = open(dir + "/" + str(id) + ".shore", "w+")
+
+		f.write(stringMap(generateRoom(shore = True)))
+
+		f.close()
+		id += 1
+
+	f = open(dir + "/" + "0.water", "w+")
+
+	f.write(stringMap(generateRoom(water = True)))
 
 printMap(generateMap(25, 4))
